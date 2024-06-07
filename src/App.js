44766d3c8f5f98aafad98d1ca26caa2f1276433d
Sam Murray <shm31094@gmail.com>
2024-06-07 15:02:03 -0700

    changing email

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     sammurray.me@gmail.com
-      </header>
+      <header className="App-header">me@sammurray.me </header>
     </div>
   );
 }
