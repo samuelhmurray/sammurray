f48ddd3ff9adbb246c6e8a3ccdf08b044f2c338c
Sam Murray <shm31094@gmail.com>
2024-06-18 11:32:03 -0400

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Under Construction</div>
       <header className="App-header">me@sammurray.me </header>
     </div>
   );
