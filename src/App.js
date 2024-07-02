c7c146728742ae27d33f2b4fe404d64f72e6b631
Sam Murray <shm31094@gmail.com>
2024-07-02 13:29:16 -0600

    cleaning link up

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-        <a className="App-header" href="mailto:me@sammurray.me">me@sammurray.me</a>
+    <div className="App-header">
+      <a className="email" href="mailto:me@sammurray.me">
+        me@sammurray.me
+      </a>
     </div>
   );
 }
