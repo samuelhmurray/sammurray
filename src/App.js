16ac4b6d60f6d614eadddefa03a585b0e61cea0e
Sam Murray <shm31094@gmail.com>
2024-07-02 13:24:01 -0600

    hello

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">me@sammurray.me </header>
+        <a className="App-header" href="mailto:me@sammurray.me">me@sammurray.me</a>
     </div>
   );
 }
