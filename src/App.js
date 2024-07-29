9fd1e845146aaaa50bffaefc6e54e0c5618f65f2
Sam Murray <shm31094@gmail.com>
2024-07-29 15:28:41 -0400

    adding text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App-header">
-      <a className="email" href="mailto:me@sammurray.me">
-        me@sammurray.me
-      </a>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Sam Murray's Personal Website</h1>
+      </header>
+      <main className="App-main">
+        <p>Feel free to reach out to me via email:</p>
+        <a className="email" href="mailto:me@sammurray.me">
+          me@sammurray.me
+        </a>
+      </main>
     </div>
   );
 }
