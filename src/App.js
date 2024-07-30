444da35cdbfac2cd0b00a6428a57442223179000
Sam Murray <shm31094@gmail.com>
2024-07-29 23:53:47 -0400

    g

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import "./App.css";
 
+const Header = () => (
+  <header className="App-header">
+    <h1>Sam Murray</h1>
+  </header>
+);
+
+const Main = () => (
+  <main className="App-main">
+    <p>Feel free to reach out to me via email:</p>
+    <a className="email" href="mailto:me@sammurray.me">
+      me@sammurray.me
+    </a>
+  </main>
+);
+
+const Footer = () => (
+  <footer className="App-footer">
+    <a 
+      href="https://www.linkedin.com/in/samuel-h-murray/" 
+      className="linkedin" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      aria-label="LinkedIn Profile"
+    >
+      <i className="fab fa-linkedin"></i>
+    </a>
+    <a 
+      href="https://github.com/samuelhmurray" 
+      className="github" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      aria-label="GitHub Profile"
+    >
+      <i className="fab fa-github"></i>
+    </a>
+  </footer>
+);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Sam Murray</h1>
-      </header>
-      <main className="App-main">
-        <p>Feel free to reach out to me via email:</p>
-        <a className="email" href="mailto:me@sammurray.me">
-          me@sammurray.me
-        </a>
-      </main>
-      <footer className="App-footer">
-        <a href="https://www.linkedin.com/in/samuel-h-murray/" className="linkedin" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="https://github.com/samuelhmurray" className="github" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i>
-        </a>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
