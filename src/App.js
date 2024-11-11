2549225550c251131a5815b5821237f6c034742b
Sam Murray <shm31094@gmail.com>
2024-11-11 14:32:36 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./App.css";
+import logo from "../public/SM.png"
 
 const Main = () => (
   <main className="App-main">
     <a className="email" href="mailto:me@sammurray.me">
-      me@sammurray.me
+    <img src={logo}/>
     </a>
   </main>
 );
