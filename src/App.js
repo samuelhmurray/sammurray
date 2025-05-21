f2d8717d763fbb979b9d1c053637585541e1bbc8
Sam Murray <shm31094@gmail.com>
2025-05-21 15:34:43 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import "./App.css";
-import logo from "./SM.png";
-
-
 
 function App() {
   return (
     <div className="App">
-    <a className="email" href="mailto:me@sammurray.me">
-      <img src={logo} alt="Sam Murray logo" />
-    </a>
+      <a className="email" href="mailto:me@sammurray.me">
+        Coming Soon
+      </a>
     </div>
   );
 }
