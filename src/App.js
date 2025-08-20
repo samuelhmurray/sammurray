1d2e52918a27426dd9975ad92d5555186e34060c
Sam Murray <shm31094@gmail.com>
2025-08-20 15:35:17 -0500

    helo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function Home() {
@@ -9,6 +9,30 @@ function Home() {
         <a className="circle" href="/about">ABOUT</a>
         <a className="circle" href="mailto:me@sammurray.me">CONTACT</a>
         <a className="circle" href="/WIP">WIP</a>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App about-page">
+      <div className="about-container">
+        <Link to="/" className="line-left"></Link>
+
+        <div className="about-inner">
+          <img 
+            src="/me.jpg"
+            alt="Sam Murray"
+            className="about-photo"
+          />
+          <div className="about-text">
+            <h1>Sam Murray</h1>
+            <p>
+              Appalachian-based designer and student focused on using honest materials to create works that are function-led. I also aim to create work that is playful and can spark joy in those who interact with it. I am equally inspired by ancient woodworking techniques as well as more modern design, such as the Bauhaus movement and Achille Castiglioni. At the core of my approach is curiosity - I believe this is one of the greatest virtues I possess and hope to continue to hone and focus that curiosity as I continue my education.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -27,7 +51,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<ComingSoon pageName="About" />} />
+        <Route path="/about" element={<About />} />
         <Route path="/WIP" element={<ComingSoon pageName="WIP" />} />
       </Routes>
     </Router>
