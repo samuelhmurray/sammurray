ffba022bb03159249de7ce179d812360b5b0b599
Sam Murray <shm31094@gmail.com>
2026-03-20 14:43:46 -0400

    about update

--- src/App.js
+++ src/App.js
@@ -35,10 +35,8 @@ function About() {
             className="about-photo"
           />
           <div className="about-text">
-            <h1>Sam Murray</h1>
             <p>
-              I am an Appalachian based designer and student focused on using honest materials to create works that are function led. I also aim to create work that is playful and can spark joy in those who interact with it. I am equally inspired by ancient woodworking techniques as well as more modern design, such as the Bauhaus movement and Achille Castiglioni. At the core of my approach is curiosity - I believe this is one of the greatest virtues I possess and hope to continue to hone and focus that curiosity as I continue my education.
-            </p>
+I am a designer and student who is interested in creating work that incites community, enables care, and invites those who interact into a deeper conversation or connection. I am influenced and inspired both by my upbringing in Appalachia as well as my hands-on experience working with metal, concrete, wood, and textiles.             </p>
           </div>
         </div>
       </div>
